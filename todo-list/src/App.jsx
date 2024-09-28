53e09bdbe01b4161d2077e1b1580a6a8a00aa2cc
sharmaskartik-coder <sharmaskartik95@gmail.com>
2024-09-28 09:26:51 +0530

    Recovered old file. Lost progress. Building Again.

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import ListToDoComponent from './components/ListToDoComponent';
+
+function App() {
+  return (
+    <>
+      <ListToDoComponent />
+    </>
+  );
+}
+
+export default App;
